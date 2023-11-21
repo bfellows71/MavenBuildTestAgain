9fd40c1b23a393e95ea8bbc05d4b7a8671abcdf2
bfellows71 <124815356+bfellows71@users.noreply.github.com>
2023-11-21 12:29:54 -0800

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Main {
+    public class PoorlyWrittenFibonacci {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter the number of Fibonacci terms you want: ");
+            int n = input.nextInt();
+
+            int firstTerm = 0;
+            int secondTerm = 1;
+            System.out.print("Fibonacci Sequence: ");
+            System.out.print(firstTerm + ", " + secondTerm + ", ");
+
+            for (int i = 2; i <= n; i++) {
+                int nextTerm = firstTerm + secondTerm;
+                System.out.print(nextTerm);
+                if (i < n) {
+                    System.out.print(", ");
+                }
+                firstTerm = secondTerm;
+                secondTerm = nextTerm;
+            }
+
+            System.out.println("The " + n + "th Fibonacci term is: " + secondTerm);
+            input.close()
+        }
+    }
+}
